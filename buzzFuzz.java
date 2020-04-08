e8f7e9bf6b1a010fe9a0246ba2eccccd42e9056d
sekhartd <59003173+sekhartd@users.noreply.github.com>
2020-04-07 22:17:11 -0500

    Add files via upload

--- buzzFuzz.java
+++ buzzFuzz.java
@@ -4,7 +4,15 @@ public class buzzFuzz {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
+		int[] array = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+		
+		for(int i = 1; i < array.length; i++ )
+			if (i%3==0 && i%5==0) {
+					System.out.println(i+ "is a buzzfuzz number"); 
+	}else if(i %3 == 0) {
+		System.out.println(i + " is a buzz number");
+	} else if(i%5 == 0) {
+		System.out.println(i + " is a fuzz number");	
+}
+}
 }
